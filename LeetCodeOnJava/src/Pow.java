ce72970415286fc7a02e0e495f2bb32e3b9146a0
Galibier <cx.sjtu@gmail.com>
2018-09-06 11:28:18 +0800

    50. Pow(x, n)

---
+++ LeetCodeOnJava/src/Pow.java
@@ -0,0 +1,11 @@
+public class Pow {
+    public double pow(double x, int n) {
+        double result = 1.0;
+        for (int i = n; i != 0; i /= 2, x *= x) {
+            if (i % 2 != 0) {
+                result *= x;
+            }
+        }
+        return n < 0 ? 1.0 / result : result;
+    }
+}
