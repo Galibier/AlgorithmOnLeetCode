a901abd5ffce3abcaf84206219e22ba2be88c94b
Galibier <cx.sjtu@gmail.com>
2018-09-07 18:47:17 +0800

    61. Rotate List

---
+++ LeetCodeOnJava/src/RotateList.java
@@ -0,0 +1,25 @@
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+
+        int length;
+        for (length = 0; fast.next != null; length++) {
+            fast = fast.next;
+        }
+
+        for (int j = length - k % length; j > 0; j--) {
+            slow = slow.next;
+        }
+
+        fast.next = dummy.next;
+        dummy.next = slow.next;
+        slow.next = null;
+
+        return dummy.next;
+    }
+}
