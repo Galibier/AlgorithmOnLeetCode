76a3b515c8dc743701297905e288bcd29c704754
Galibier <cx.sjtu@gmail.com>
2018-09-09 00:55:50 +0800

    99. Recover Binary Search Tree

--- LeetCodeOnJava/src/RecoverBinarySearchTree.java
+++ LeetCodeOnJava/src/RecoverBinarySearchTree.java
@@ -1,29 +1,30 @@
+import java.util.Stack;
+
 public class RecoverBinarySearchTree {
-    TreeNode firstElement = null;
-    TreeNode secondElement = null;
-    TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
-
     public void recoverTree(TreeNode root) {
-        traverse(root);
-        int temp = firstElement.val;
-        firstElement.val = secondElement.val;
-        secondElement.val = temp;
-    }
-
-    private void traverse(TreeNode root) {
-        if (root == null) {
-            return;
+        TreeNode first = null;
+        TreeNode second = null;
+        TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= prev.val) {
+                if (first == null) {
+                    first = prev;
+                    second = root;
+                } else {
+                    second = root;
+                }
+            }
+            prev = root;
+            root = root.right;
         }
-
-        traverse(root.left);
-
-        if (firstElement == null && prevElement.val >= root.val) {
-            firstElement = prevElement;
-        }
-        if (firstElement != null && prevElement.val >= root.val) {
-            secondElement = root;
-        }
-        prevElement = root;
-        traverse(root.right);
+        int tmp = first.val;
+        first.val = second.val;
+        second.val = tmp;
     }
 }
