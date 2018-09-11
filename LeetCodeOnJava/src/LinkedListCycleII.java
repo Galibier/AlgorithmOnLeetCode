c68b36c9f0de57460e2d1480830317432af0033e
Galibier <cx.sjtu@gmail.com>
2018-09-12 00:33:33 +0800

    142. Linked List Cycle II

---
+++ LeetCodeOnJava/src/LinkedListCycleII.java
@@ -0,0 +1,21 @@
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
