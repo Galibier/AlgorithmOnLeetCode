62595cb7e7117017c57db3b7efc0633cacd6cf22
Galibier <cx.sjtu@gmail.com>
2018-10-17 20:14:46 +0800

    fast new version

--- LeetCodeOnCpp/1.cpp
+++ LeetCodeOnCpp/1.cpp
@@ -1,28 +1,18 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		vector<int> tmp = nums;
-		sort(tmp.begin(), tmp.end());
-		int index1 = 0, index2 = tmp.size() - 1;
-
-		while (index1 < index2) {
-			if (tmp[index1] + tmp[index2] > target) 
-				index2--;
-			else if (tmp[index1] + tmp[index2] < target) 
-				index1++;
-			else 
-				break;
-		}
-
-		vector<int> ret;
-		for (int i = 0; i < nums.size(); i++) {
-			if (nums[i] == tmp[index1])
-				ret.push_back(i);
-			else if (nums[i] == tmp[index2])
-				ret.push_back(i);
-		}
-		if (ret[0] > ret[1])
-			swap(ret[0], ret[1]);
-		return ret;
-	}
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(target-nums[i])==mp.end()){
+                mp[nums[i]]=i;
+            }
+            else{
+                return vector<int>({mp[target-nums[i]], i});
+            }
+        }
+    }
 };
