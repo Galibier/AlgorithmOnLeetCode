150019d2bf8418a1de3550a4e12b4ca1e3fec6ec
Galibier <cx.sjtu@gmail.com>
2018-10-18 00:06:41 +0800

    fast new version

--- LeetCodeOnCpp/2.cpp
+++ LeetCodeOnCpp/2.cpp
@@ -12,18 +12,18 @@ class Solution {
 public:
 	ListNode * addTwoNumbers(ListNode* l1, ListNode* l2) {
 		ListNode preHead(0), *head = &preHead;
-		int extra = 0;
-		while (l1 != nullptr || l2 != nullptr || extra != 0) {
+		int carry = 0;
+		while (l1 != nullptr || l2 != nullptr || carry != 0) {
 			if (l1 != nullptr) {
-				extra += l1->val;
+				carry += l1->val;
 				l1 = l1->next;
 			}
 			if (l2 != nullptr) {
-				extra += l2->val;
+				carry += l2->val;
 				l2 = l2->next;
 			}
-			head->next = new ListNode(extra % 10);
-			extra /= 10;
+			head->next = new ListNode(carry % 10);
+			carry /= 10;
 			head = head->next;
 		}
 		return preHead.next;
--- LeetCodeOnCpp/3.cpp
+++ LeetCodeOnCpp/3.cpp
@@ -1,23 +1,21 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	int lengthOfLongestSubstring(string s) {
-		if (s.size() == 0)
-			return 0;
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0)
+            return 0;
 
-		vector<int> dp(s.size() + 1);
-		dp[0] = 1;
-		for (int i = 1; i < s.size(); i++) {
-			int j = 1;
-			for (; j < dp[i - 1]; j++) {
-				if (s[i] == s[i - j])
-					break;
-			}
-			if (s[i] == s[i - j])
-				dp[i] = j;
-			else
-				dp[i] = dp[i - 1] + 1;
-		}
+        int res = 0, begin = -1;
+        vector<int> idx(256, -1);
+        for (int i = 0; i != s.size(); i++) {
+            if (idx[s[i]] > begin) {
+                begin = idx[s[i]];
+            }
+            idx[s[i]] = i;
+            res = max(res, i - begin);
+        }
 
-		return *max_element(dp.begin(), dp.end());
-	}
+        return res;
+    }
 };
