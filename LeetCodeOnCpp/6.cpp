92ee0d573f09535ed9ae155dec5ece10817e4f05
Galibier <cx.sjtu@gmail.com>
2018-10-18 00:27:23 +0800

    fast new version

--- LeetCodeOnCpp/6.cpp
+++ LeetCodeOnCpp/6.cpp
@@ -1,17 +1,19 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	string convert(string s, int numRows) {
-		if (numRows <2 || s.size() == 0)
-			return s;
-		int count = numRows * 2 - 2;
-		vector<string> ret(numRows);
-		for (int i = 0; i < s.size(); i++) {
-			ret[min(i%count, count - i % count)] += s.substr(i, 1);
-		}
-		string ans;
-		for (int i = 0; i < ret.size(); i++) {
-			ans += ret[i];
-		}
-		return ans;
-	}
+    string convert(string s, int numRows) {
+        if (numRows < 2 || s.size() == 0)
+            return s;
+        int count = numRows * 2 - 2;
+        vector<string> ret(numRows);
+        for (int i = 0; i < s.size(); i++) {
+            ret[min(i % count, count - i % count)] += s[i];
+        }
+        string ans;
+        for (auto str:ret) {
+            ans += str;
+        }
+        return ans;
+    }
 };
