a088325d0b15ab7255485bc1c5432fbaa5f15ca0
Galibier <cx.sjtu@gmail.com>
2018-10-19 00:41:12 +0800

    faster new version

--- LeetCodeOnCpp/19.cpp
+++ LeetCodeOnCpp/19.cpp
@@ -1,3 +1,5 @@
+#include "head_file.h"
+
 /**
 * Definition for singly-linked list.
 * struct ListNode {
@@ -6,22 +8,20 @@
 *     ListNode(int x) : val(x), next(NULL) {}
 * };
 */
-class Solution
-{
+class Solution {
 public:
-	ListNode * removeNthFromEnd(ListNode* head, int n)
-	{
-		ListNode** t1 = &head, *t2 = head;
-		for (int i = 1; i < n; ++i)
-		{
-			t2 = t2->next;
-		}
-		while (t2->next != NULL)
-		{
-			t1 = &((*t1)->next);
-			t2 = t2->next;
-		}
-		*t1 = (*t1)->next;
-		return head;
-	}
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode preHead(0);
+        preHead.next = head;
+        ListNode *t1 = &preHead, *t2 = head;
+        for (int i = 1; i < n; ++i) {
+            t2 = t2->next;
+        }
+        while (t2->next != NULL) {
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        t1->next = t1->next->next;
+        return preHead.next;
+    }
 };
