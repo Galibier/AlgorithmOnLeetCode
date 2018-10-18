edc6b9715dcda1064271744bc0bd9d4b77c7f8d1
Galibier <cx.sjtu@gmail.com>
2018-10-18 23:23:36 +0800

    faster new version

--- LeetCodeOnCpp/10.cpp
+++ LeetCodeOnCpp/10.cpp
@@ -1,35 +1,34 @@
-class Solution {
-public:
-	bool isMatch(string s, string p) {
-		int m = s.size(), n = p.size();
-		vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
-		
-		f[0][0] = true;
-		for (int i = 1; i <= m; i++)
-			f[i][0] = false;
-		for (int j = 1; j <= n; j++) 
-			f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
-		
-		for (int i = 1; i <= m; i++)
-			for (int j = 1; j <= n; j++)
-				if (p[j - 1] != '*')
-					f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
-				else
-					f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
-
-		return f[m][n];
-	}
-};
+#include "head_file.h"
 
 class Solution {
 public:
-	bool isMatch(string s, string p) {
-		if (p.empty()) 
-			return s.empty();
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
 
-		if ('*' == p[1])
-			return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
-		else
-			return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
-	}
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++)
+            dp[i][0] = false;
+        for (int j = 1; j <= n; j++)
+            dp[0][j] = j > 1 && '*' == p[j - 1] && dp[0][j - 2];
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] != '*')
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                else
+                    dp[i][j] = dp[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && dp[i - 1][j];
+
+        return dp[m][n];
+    }
+
+    bool isMatch_recursive(string s, string p) {
+        if (p.empty())
+            return s.empty();
+
+        if ('*' == p[1])
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
 };
--- LeetCodeOnCpp/7.cpp
+++ LeetCodeOnCpp/7.cpp
@@ -1,18 +1,20 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	int reverse(int x) {
-		bool flag = true;
-		if (x < 0)
-			flag = false;
-		long long num = abs(x), ret = 0;
-		while (num) {
-			ret = ret * 10 + num % 10;
-			num /= 10;
-		}
-		int res = (int)ret;
-		if (ret != res)
-			return 0;
-		else
-			return (flag ? 1 : -1)*ret;
-	}
+    int reverse(int x) {
+        bool flag = true;
+        if (x < 0)
+            flag = false;
+        long long num = abs(x), ret = 0;
+        while (num) {
+            ret = ret * 10 + num % 10;
+            num /= 10;
+        }
+        int res = (int) ret;
+        if (ret != res)
+            return 0;
+        else
+            return (flag ? 1 : -1) * ret;
+    }
 };
