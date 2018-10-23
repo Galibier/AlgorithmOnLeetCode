f590e01a01b31a090e79473af0e4d7efb85bc59b
Galibier <cx.sjtu@gmail.com>
2018-10-23 22:12:21 +0800

    fast new version

--- LeetCodeOnCpp/66.cpp
+++ LeetCodeOnCpp/66.cpp
@@ -1,19 +1,20 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	vector<int> plusOne(vector<int>& digits) {
-		int carry = 1;
-		for (int i = digits.size() - 1; i >= 0; i--) {
-			if (digits[i] == 9 && carry == 1) {
-				digits[i] = 0;
-				carry = 1;
-			}
-			else {
-				digits[i] += carry;
-				return digits;
-			}
-		}
-		digits[0] = 1;
-		digits.push_back(0);
-		return digits;
-	}
+    vector<int> plusOne(vector<int> &digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] == 9 && carry == 1) {
+                digits[i] = 0;
+                carry = 1;
+            } else {
+                digits[i] += carry;
+                return digits;
+            }
+        }
+        digits[0] = 1;
+        digits.push_back(0);
+        return digits;
+    }
 };
