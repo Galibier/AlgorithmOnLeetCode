61e4175ad783152e56f6479e8bee04ac85c22336
Galibier <cx.sjtu@gmail.com>
2018-10-23 23:29:26 +0800

    fast new version

--- LeetCodeOnCpp/77.cpp
+++ LeetCodeOnCpp/77.cpp
@@ -1,22 +1,24 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	vector<vector<int> > combine(int n, int k) {
-		vector<vector<int> >res;
-		if (n < k)return res;
-		vector<int> temp(0, k);
-		combine(res, temp, 0, 0, n, k);
-		return res;
-	}
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        if (n < k)return res;
+        vector<int> temp(0, k);
+        combine(res, temp, 0, 0, n, k);
+        return res;
+    }
 
-	void combine(vector<vector<int> > &res, vector<int> &temp, int start, int cnt, int n, int k) {
-		if (cnt == k) {
-			res.push_back(temp);
-			return;
-		}
-		for (int i = start; i < n; i++) {
-			temp.push_back(i + 1);
-			combine(res, temp, i + 1, cnt + 1, n, k);
-			temp.pop_back();
-		}
-	}
+    void combine(vector<vector<int>> &res, vector<int> &temp, int start, int cnt, int n, int k) {
+        if (cnt == k) {
+            res.push_back(temp);
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            temp.push_back(i + 1);
+            combine(res, temp, i + 1, cnt + 1, n, k);
+            temp.pop_back();
+        }
+    }
 };
--- LeetCodeOnCpp/78.cpp
+++ LeetCodeOnCpp/78.cpp
@@ -1,16 +1,18 @@
+#include "head_file.h"
+
 class Solution {
 public:
-	vector<vector<int>> subsets(vector<int> &nums) {
-		sort(nums.begin(), nums.end());
-		int len = nums.size();
-		int cnt = pow(2, len);
-		vector<vector<int>> set(cnt, vector<int>());
-		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < cnt; j++) {
-				if ((j >> i) & 1)
-					set[j].push_back(nums[i]);
-			}
-		}
-		return set;
-	}
+    vector<vector<int>> subsets(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        int len = nums.size();
+        int cnt = pow(2, len);
+        vector<vector<int>> set(cnt, vector<int>());
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < cnt; j++) {
+                if ((j >> i) & 1)
+                    set[j].push_back(nums[i]);
+            }
+        }
+        return set;
+    }
 };
