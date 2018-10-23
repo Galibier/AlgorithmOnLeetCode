cb4b6e29629b4e9943ecbfaa114e8846ac2848e5
Galibier <cx.sjtu@gmail.com>
2018-10-23 15:30:11 +0800

    fast new version

--- LeetCodeOnCpp/57.cpp
+++ LeetCodeOnCpp/57.cpp
@@ -1,3 +1,5 @@
+#include "head_file.h"
+
 /**
 * Definition for an interval.
 * struct Interval {
@@ -9,20 +11,20 @@
 */
 class Solution {
 public:
-	vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
-		auto compare = [](const Interval &intv1, const Interval &intv2)
-		{	return intv1.end < intv2.start; };
-		auto range = equal_range(intervals.begin(), intervals.end(), newInterval, compare);
-		auto itr1 = range.first, itr2 = range.second;
-		if (itr1 == itr2) {
-			intervals.insert(itr1, newInterval);
-		}
-		else {
-			itr2--;
-			itr2->start = min(newInterval.start, itr1->start);
-			itr2->end = max(newInterval.end, itr2->end);
-			intervals.erase(itr1, itr2);
-		}
-		return intervals;
-	}
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        auto compare = [](const Interval &intv1, const Interval &intv2) {
+            return intv1.end < intv2.start;
+        };
+        auto range = equal_range(intervals.begin(), intervals.end(), newInterval, compare);
+        auto itr1 = range.first, itr2 = range.second;
+        if (itr1 == itr2) {
+            intervals.insert(itr1, newInterval);
+        } else {
+            itr2--;
+            itr2->start = min(newInterval.start, itr1->start);
+            itr2->end = max(newInterval.end, itr2->end);
+            intervals.erase(itr1, itr2);
+        }
+        return intervals;
+    }
 };
